bb4d6b58269e885733672929940b284e8b695065
Kai Altwicker <altwicker@tallaldiproduction.de>
2025-09-05 16:40:00 +0200

    MODIFIED: adjusted dir option (WIP)

--- demo_modified.py
+++ demo_modified.py
@@ -37,7 +37,7 @@ def save_results_csv(
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument('-d','--dir', nargs='+', type=Path, default=None)
+group.add_argument('-d','--dir', type=Path, default=None)
 group.add_argument('-f','--file', default=None)
 
 parser.add_argument('-m','--model_path', type=str, default='weights/blur_jpg_prob0.5.pth')
@@ -50,9 +50,8 @@ parser.add_argument('-j','--workers', type=int, default=4, help='number of worke
 opt = parser.parse_args()
 
 if opt.dir is not None:
-  for d in opt.dir:
-    assert(d.exists()), 'Directory %s does not exist'%str(d)
-    assert(d.is_dir()), '%s is not a directory'%str(d)
+  assert(opt.dir.exists()), 'Directory %s does not exist'%str(opt.dir)
+  assert(opt.dir.is_dir()), '%s is not a directory'%str(opt.dir)
 
 
 model = resnet50(num_classes=1)
@@ -86,29 +85,31 @@ if opt.file is not None:
 
   print('probability of being synthetic: {:.2f}%'.format(prob * 100))
 
-  if opt.dir is not None:
-    dataset = datasets.ImageFolder(opt.dir, transform=trans)
-    
-    data_loader=torch.utils.data.DataLoader(dataset,
-                                              batch_size=opt.batch_size,
-                                              shuffle=False,
-                                              num_workers=opt.workers)
+if opt.dir is not None:
+  dataset = datasets.ImageFolder(opt.dir, transform=trans)
+  
+  data_loader=torch.utils.data.DataLoader(dataset,
+                                            batch_size=opt.batch_size,
+                                            shuffle=False,
+                                            num_workers=opt.workers)
 
-    y_true, y_pred = [], []
-    with torch.no_grad():
-      for data, label in tqdm(data_loader):
+  y_true, y_pred = [], []
+  with torch.no_grad():
+    for data, label in tqdm(data_loader):
 
-        # Convert numeric labels to class names
-        class_names = [dataset.classes[idx] for idx in label.flatten().tolist()]
+      # Convert numeric labels to class names
+      class_names = [dataset.classes[idx] for idx in label.flatten().tolist()]
 
-        # Prefix each class name with opt.dir.name
-        prefixed_names = [f"{opt.dir.name}_{name}" for name in class_names]
-        y_true.extend(prefixed_names)
-        if(not opt.use_cpu):
-            data = data.cuda()
-        y_pred.extend(model(data).sigmoid().flatten().tolist())
-    
-    if opt.output_csv is not None:
-      save_results_csv(opt.output_csv / f'{opt.dir.name}', y_true, y_pred)
-      print(f"Results saved to {opt.output_csv}")
+      # Prefix each class name with opt.dir.name
+      prefixed_names = [f"{opt.dir.name}_{name}" for name in class_names]
+      y_true.extend(prefixed_names)
+      if(not opt.use_cpu):
+          data = data.cuda()
+      y_pred.extend(model(data).sigmoid().flatten().tolist())
+  
+  if opt.output_csv is not None:
+    save_results_csv(opt.output_csv / f'{opt.dir.name}', y_true, y_pred)
+    print(f"Results saved to {opt.output_csv}")
+  else:
+    save_results_csv(opt.dir / f'results_{opt.dir.name}.csv', y_true, y_pred)
 
